5ef564d790006e829fa494e0ac42f7269cddab00
yv1es <yvesinglin@gmail.com>
2024-03-10 10:00:56 +0100

    single leg inverse kinematics

--- src/sixlg_motor_controller/include/sixlg_motor_controller/MotorController.hpp
+++ src/sixlg_motor_controller/include/sixlg_motor_controller/MotorController.hpp
@@ -14,8 +14,9 @@ public:
     ~MotorController() = default; 
 
 private:
+    void moveLegXYZ(double X, double Y, double Z); 
     void topic_callback(const sixlg_interfaces::msg::ServoAngles msg);
-    void writeServoAngles(const std::array<int32_t, SERVO_COUNT> angles); 
+    void writeServoAngles(const std::array<double, SERVO_COUNT> angles); 
 
     rclcpp::Subscription<sixlg_interfaces::msg::ServoAngles>::SharedPtr subscription;
     mn::CppLinuxSerial::SerialPort serialPort; 
--- src/sixlg_motor_controller/src/MotorController.cpp
+++ src/sixlg_motor_controller/src/MotorController.cpp
@@ -2,7 +2,9 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <cmath>
 #include <sstream>
+#include <stdint.h>
 #include <string>
 #include <thread>
 
@@ -18,20 +20,60 @@ MotorController::MotorController()
 
     serialPort.Open();
     std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Arduino resets after opening serial connection
+
+    const auto delay = std::chrono::milliseconds(500); 
+    while (true)
+    {
+        moveLegXYZ(0.07, 0.13, -0.06); 
+        std::this_thread::sleep_for(delay); 
+
+        moveLegXYZ(0.0, 0.10, -0.01); 
+        std::this_thread::sleep_for(delay); 
+
+        moveLegXYZ(-0.07, 0.07, -0.06); 
+        std::this_thread::sleep_for(delay); 
+    }
+}
+
+void moveLegTrajectory()
+
+
+void MotorController::moveLegXYZ(double X, double Y, double Z)
+{
+    double lF = 0.09; // length Femur
+    double lT = 0.14; // length Tibia
+
+    double lF_sqr = std::pow(lF, 2);
+    double lT_sqr = std::pow(lF, 2);
+
+    double L_sqr = std::pow(X, 2) + std::pow(Y, 2);
+    double l_sqr = L_sqr + std::pow(Z, 2);
+    double l = std::sqrt(l_sqr);
+
+    double J1 = std::atan(X / Y) + M_PI / 2;
+
+    double J2 = std::acos((lF_sqr + l_sqr - lT_sqr) / (2 * lF * l)) - std::atan(-Z / l) + M_PI/2;
+
+    double J3 = std::acos((lF_sqr + lT_sqr - l_sqr) / (2 * lF * lT));
+
+    RCLCPP_INFO(this->get_logger(), "Target XYZ = %f; %f; %f; => J1=%f   J2=%f   J3=%f   ", X, Y, Z, J1, J2, J3);
+    writeServoAngles({J1, J2, J3}); 
 }
 
 void MotorController::topic_callback(const sixlg_interfaces::msg::ServoAngles msg)
 {
-    writeServoAngles(msg.angles);
+
+    // writeServoAngles(msg.angles);
 }
 
-void MotorController::writeServoAngles(const std::array<int32_t, SERVO_COUNT> angles)
+void MotorController::writeServoAngles(const std::array<double, SERVO_COUNT> angles)
 {
     std::ostringstream strm;
     strm << "<";
     for (const auto angle : angles)
     {
-        strm << angle << ",";
+        int32_t deg = angle * 180 / M_PI; 
+        strm << deg << ",";
     }
     strm.seekp(-1, strm.cur);
     strm << ">";
